49d793d240d69cef29ec2610264e7e1b992dae64
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-10-31 10:59:18 +0100

    add irssi config

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd
 REPO=dotfiles
 BASE_PATH=$(pwd)
 REPO_PATH=$BASE_PATH/$REPO
-FILES=( .gitconfig .gitignore_global .tmux.conf .vim .vimrc .zsh_aliases .zsh_autocomp .zshrc .oh-my-zsh)
+FILES=( .gitconfig .gitignore_global .tmux.conf .vim .vimrc .zsh_aliases .zsh_autocomp .zshrc .oh-my-zsh .irssi)
 BKP_DIR=dotfiles-backup
 
 # Create a backup dir.
